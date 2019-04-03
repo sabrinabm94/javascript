366a36e2251a61bc737f5c4cdf1df5685f4dfab1
sabrina.moreira@neogrid.com <sabrinabgbc@email.com>
2019-04-03 16:10:01 -0300

    atualização de comentários

--- angular/gif-search/src/app/gif-search/gif-search.component.spec.ts
+++ angular/gif-search/src/app/gif-search/gif-search.component.spec.ts
@@ -1,5 +1,13 @@
-//fdescribe
+//fdescribe ou fit
 //vai só rodar todos os testes 9it) que existen dentro deste describe
+
+//xdescribe ou xit
+//ignorar testes ou grupo de testes
+
+beforeEach(() => {
+  //resete de variáveis antes de iniciar outro it
+  //instância de objeto
+});
 
 fdescribe('GifSearchService', () => {
   it("2 deveria ser igual a 2", () => {
