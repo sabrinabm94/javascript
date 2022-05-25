aae46e6069c0a9e962500b74569f9d23422e8080
Sabrina B. M <sabrinabmoreira@gmail.com>
2022-05-25 18:34:07 -0300

    implementação de variáveis de ambiente

--- node/readme.txt
+++
@@ -1,5 +0,0 @@
-# Node
-
-## Development server
-json-server --watch db.json --port 3004
-http://localhost:3004/
--- react/website/src/init-firebase.js
+++ react/website/src/init-firebase.js
@@ -6,14 +6,14 @@ import { getStorage } from "firebase/storage";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCh2jvb0ttUJJaWI1-_8DZ-0DAeJJK90Jk",
-    authDomain: "website-sabrinabm94.firebaseapp.com",
-    databaseURL: "https://website-sabrinabm94-default-rtdb.firebaseio.com",
-    projectId: "website-sabrinabm94",
-    storageBucket: "website-sabrinabm94.appspot.com",
-    messagingSenderId: "769442502027",
-    appId: "1:769442502027:web:0dc4187928005e09438b8e",
-    measurementId: "G-97FEKHF5GF"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
