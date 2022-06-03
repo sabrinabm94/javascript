ed4b850cf11e90a6534021689b997bce1fa5a8f8
Sabrina <sabrinabm94@gmail.com>
2022-06-02 22:49:49 -0300

    início da implementação da função de gravar dados do form, utilização do ref

--- react/website/src/pages/admin/Portfolio.js
+++ react/website/src/pages/admin/Portfolio.js
@@ -2,48 +2,66 @@ import React, { Component } from "react";
 
 class Portfolio extends Component {
     constructor(props) {
-        super(props)
+        super(props);
     }
+
+    async setData(collection, title, description, image) {
+        event.preventDefault();
+
+        if(collection) {
+            console.log("collection", collection);
+
+            if(title && description) {
+                console.log("title ", title.value);
+                console.log("description ", description.value);
+            }
+
+            if(image) {
+                console.log("image ", image.value);
+            }
+        }
+    }
+
     render() {
         return (
             <section id="admin-portfolio" className="container-fluid text-center bg-grey">
                 <h2>Portfolio</h2>
-                <form method="post" className="form">
+                <form onSubmit={() => this.setData("thumbnailElements", this.portfolioTitle, this.portfolioDescription, this.portfolioImage)} className="form">
                     <div className="row">
                         <div className="col-6 col-sm-6 col-md-6 col-lg-6">
                             <div className="form-group">
-                                <label for="title">Title</label>
-                                <input type="text" className="form-control" id="title" placeholder="title" />
+                                <label htmlFor="title">Title</label>
+                                <input type="text" className="form-control" id="portfolioTitle" placeholder="Title" ref={portfolioTitle => (this.portfolioTitle = portfolioTitle)} /> 
                             </div>
                         </div>
                         <div className="col-6 col-sm-6 col-md-6 col-lg-6">
                             <div className="form-group">
-                                <label for="description">Description</label>
-                                <input type="text" className="form-control" id="description" placeholder="description" />
+                                <label htmlFor="description">Description</label>
+                                <input type="text" className="form-control" id="portfolioDescription" placeholder="Description" ref={portfolioDescription => (this.portfolioDescription = portfolioDescription)} /> 
                             </div>
                         </div>
                         <div className="col-12 col-sm-12 col-md-12 col-lg-12">
                             <div className="form-group">
-                                <label for="img"></label>
-                                <input type="file" id="img" name="img" accept="image/*"></input>
+                                <label htmlFor="image"></label>
+                                <input type="file" name="image" accept="image/*" id="portfolioImage" placeholder="Image" ref={portfolioImage => (this.portfolioImage = portfolioImage)} /> 
                             </div>
                             <button type="submit" className="btn btn-primary">Enviar</button>
                         </div>
                     </div>
                 </form>
                 <h2>Costumers</h2>
-                <form method="post" className="form">
+                <form onSubmit={() => this.setData("carouselElements", this.carouselTitle, this.carouselDescription)} className="form">
                     <div className="row">
                         <div className="col-6 col-sm-6 col-md-6 col-lg-6">
                             <div className="form-group">
-                                <label for="title">Title</label>
-                                <input type="text" className="form-control" id="title" placeholder="title" />
+                                <label htmlFor="title">Title</label>
+                                <input type="text" className="form-control" id="carouselTitle" placeholder="Title" ref={carouselTitle => (this.carouselTitle = carouselTitle)} /> 
                             </div>
                         </div>
                         <div className="col-6 col-sm-6 col-md-6 col-lg-6">
                             <div className="form-group">
-                                <label for="description">Description</label>
-                                <input type="text" className="form-control" id="description" placeholder="description" />
+                                <label htmlFor="description">Description</label>
+                                <input type="text" className="form-control" id="carouselDescription" placeholder="Description" ref={carouselDescription => (this.carouselDescription = carouselDescription)} /> 
                             </div>
                         </div>
                         <div className="col-12 col-sm-12 col-md-12 col-lg-12">
