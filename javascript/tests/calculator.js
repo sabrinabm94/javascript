4cd70a2137ebb571bd7dc4718f3dbd4e3720304d
Sabrina <sabrinabm94@gmail.com>
2022-09-12 17:57:02 -0300

    refinamento de função de soma

--- javascript/tests/calculator.js
+++ javascript/tests/calculator.js
@@ -1,3 +1,10 @@
 function sum(a, b) {
-    return a + b;
+    const numberOne = parseInt(a);
+    const numberTwo = parseInt(b);
+
+    if(Number.isNaN(numberOne) && Number.isNaN(numberTwo)) {
+        throw new Error("Check your inputs and try again");
+    }
+    
+    return numberOne + numberTwo;
 }
